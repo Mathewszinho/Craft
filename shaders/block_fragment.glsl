9bb9c026d5dd13491e462bd7dda98c608385a7b9
Mathews <86793169+Mathewszinho@users.noreply.github.com>
2022-07-15 17:54:15 -0300

    Update block_fragment.glsl

--- shaders/block_fragment.glsl
+++ shaders/block_fragment.glsl
@@ -1,39 +1,54 @@
 #version 120
-
 uniform sampler2D sampler;
 uniform sampler2D sky_sampler;
 uniform float timer;
 uniform float daylight;
 uniform int ortho;
-
 varying vec2 fragment_uv;
 varying float fragment_ao;
 varying float fragment_light;
 varying float fog_factor;
 varying float fog_height;
 varying float diffuse;
-
-const float pi = 3.14159265;
-
-void main() {
-    vec3 color = vec3(texture2D(sampler, fragment_uv));
-    if (color == vec3(1.0, 0.0, 1.0)) {
-        discard;
-    }
-    bool cloud = color == vec3(1.0, 1.0, 1.0);
-    if (cloud && bool(ortho)) {
-        discard;
-    }
-    float df = cloud ? 1.0 - diffuse * 0.2 : diffuse;
-    float ao = cloud ? 1.0 - (1.0 - fragment_ao) * 0.2 : fragment_ao;
-    ao = min(1.0, ao + fragment_light);
-    df = min(1.0, df + fragment_light);
-    float value = min(1.0, daylight + fragment_light);
-    vec3 light_color = vec3(value * 0.3 + 0.2);
-    vec3 ambient = vec3(value * 0.3 + 0.2);
-    vec3 light = ambient + light_color * df;
-    color = clamp(color * light * ao, vec3(0.0), vec3(1.0));
-    vec3 sky_color = vec3(texture2D(sky_sampler, vec2(timer, fog_height)));
-    color = mix(color, sky_color, fog_factor);
-    gl_FragColor = vec4(color, 1.0);
+void main ()
+{
+  vec4 tmpvar_1;
+  tmpvar_1 = texture2D (sampler, fragment_uv);
+  if ((tmpvar_1.xyz == vec3(1.0, 0.0, 1.0))) {
+    discard;
+  };
+  bool tmpvar_2;
+  tmpvar_2 = (tmpvar_1.xyz == vec3(1.0, 1.0, 1.0));
+  if ((tmpvar_2 && bool(ortho))) {
+    discard;
+  };
+  float tmpvar_3;
+  if (tmpvar_2) {
+    tmpvar_3 = (1.0 - (diffuse * 0.2));
+  } else {
+    tmpvar_3 = diffuse;
+  };
+  float tmpvar_4;
+  if (tmpvar_2) {
+    tmpvar_4 = (1.0 - ((1.0 - fragment_ao) * 0.2));
+  } else {
+    tmpvar_4 = fragment_ao;
+  };
+  float tmpvar_5;
+  tmpvar_5 = min (1.0, (daylight + fragment_light));
+  vec2 tmpvar_6;
+  tmpvar_6.x = timer;
+  tmpvar_6.y = fog_height;
+  vec4 tmpvar_7;
+  tmpvar_7.w = 1.0;
+  tmpvar_7.xyz = mix (clamp ((
+    (tmpvar_1.xyz * (vec3(((tmpvar_5 * 0.3) + 0.2)) + (vec3((
+      (tmpvar_5 * 0.3)
+     + 0.2)) * min (1.0, 
+      (tmpvar_3 + fragment_light)
+    ))))
+   * 
+    min (1.0, (tmpvar_4 + fragment_light))
+  ), vec3(0.0, 0.0, 0.0), vec3(1.0, 1.0, 1.0)), texture2D (sky_sampler, tmpvar_6).xyz, fog_factor);
+  gl_FragColor = tmpvar_7;
 }
