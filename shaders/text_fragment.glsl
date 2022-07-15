c10135156b3302aa3d88b9e359a812129599c2c3
Mathews <86793169+Mathewszinho@users.noreply.github.com>
2022-07-15 17:58:19 -0300

    Update text_fragment.glsl

--- shaders/text_fragment.glsl
+++ shaders/text_fragment.glsl
@@ -1,19 +1,19 @@
 #version 120
-
 uniform sampler2D sampler;
 uniform bool is_sign;
-
 varying vec2 fragment_uv;
-
-void main() {
-    vec4 color = texture2D(sampler, fragment_uv);
-    if (is_sign) {
-        if (color == vec4(1.0)) {
-            discard;
-        }
-    }
-    else {
-        color.a = max(color.a, 0.4);
-    }
-    gl_FragColor = color;
+void main ()
+{
+  vec4 color_1;
+  vec4 tmpvar_2;
+  tmpvar_2 = texture2D (sampler, fragment_uv);
+  color_1 = tmpvar_2;
+  if (is_sign) {
+    if ((tmpvar_2 == vec4(1.0, 1.0, 1.0, 1.0))) {
+      discard;
+    };
+  } else {
+    color_1.w = max (tmpvar_2.w, 0.4);
+  };
+  gl_FragColor = color_1;
 }
